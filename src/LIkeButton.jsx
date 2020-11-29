7dcc232df048440081199993426ff40b7e28559d
shuntagami <shuntagami23@gmail.com>
2020-11-29 11:11:39 +0900

    finished #7

---
+++ src/LIkeButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LIkeButton = (props) => {
+  return (
+    <button id={"counter"}>いいね数：{props.count}</button>
+  )
+}
+
+export default LIkeButton
---
+++ src/components/ForBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function For() {
+  return (
+    <h2>For!!</h2>
+  )
+}
+
+export const Bar = () => {
+  return (
+    <h2>Bar!</h2>
+  )
+}
