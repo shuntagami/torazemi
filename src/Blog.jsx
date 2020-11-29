66ba66b753f113529798965b40f0b63c52807cb3
shuntagami <shuntagami23@gmail.com>
2020-11-29 11:17:52 +0900

    classComponent→ReactHocks
    削除

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,56 +3,40 @@ import Article from './Article'
 import * as ForBar from './components/ForBar'
 import Hoge from './components/Hoge'
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count:  0
-    }
-  }
+const Blog = () => {
 
-  componentDidMount() {
-    // ボタンがクリックされたらいいねをカウントアップする
-    document.getElementById("counter").addEventListener('click', this.countUp)
-  }
+  // componentDidMount() {
+  //   // ボタンがクリックされたらいいねをカウントアップする
+  //   document.getElementById("counter").addEventListener('click', this.countUp)
+  // }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState({count: 0})
-    }
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //     this.setState({count: 0})
+  //   }
+  // }
 
-  componentWillUnmount() {
-    document.getElementById("counter").removeEventListener('click', this.countUp)
-  }
+  // componentWillUnmount() {
+  //   document.getElementById("counter").removeEventListener('click', this.countUp)
+  // }
 
-  // 公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  };
+  // countUp = () => {
+  //   this.setState({count: this.state.count + 1})
+  // }
 
-  countUp = () => {
-    this.setState({count: this.state.count + 1})
-  }
-
-  render() {
     const authorName = "shuntagami"
     return (
       <>
         <Article
-          title={"Reactの使い方"} isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
+          title={"Reactの使い方"}
+          // count={this.state.count}
         />
         <ForBar.For />
         <ForBar.Bar />
         <Hoge />
       </>
     )
-  }
+
 }
 
 export default Blog
